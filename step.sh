7a3fefee065df6e1d21a5373491b572bccc1caf5
DamienBitrise <damien.murphy@bitrise.io>
2022-09-19 19:09:45 -0700

    Initial Commit

---
+++ step.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing Anka Scan..."
+
+FULL_FILE_NAME=$(echo $(curl -Ls -r 0-1 -o /dev/null -w %{url_effective} https://veertu.com/downloads/runtime-mac-scan) | cut -d/ -f5)
+PARTIAL_FILE_NAME=$(echo $FULL_FILE_NAME | awk -F'.zip' '{print $1}')
+curl -Ls https://veertu.com/downloads/runtime-mac-scan -o $FULL_FILE_NAME
+unzip $FULL_FILE_NAME
+rm -f $FULL_FILE_NAME
+
+cd $PARTIAL_FILE_NAME
+
+./runtime-mac-scan-server &
+
+sleep 5
+
+./runtime-mac-scan-cli start
+
+./runtime-mac-scan-cli status
